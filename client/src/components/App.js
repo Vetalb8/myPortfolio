76f2695c64eb81b96a8f25dbb42e863ce99cb108
Vitaliy Kudla <vkudla@scentbird.com>
2018-04-26 13:02:20 +0400

    init main and login pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,56 +1,20 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import { increment } from '../modules/counter'
+import MainPage from '../pages/MainPage'
+import LoginPage from '../pages/LoginPage'
 
 
-@connect((state) => ({
-  counter: state.counter,
-}), { increment })
 export default class App extends Component {
 
-  static propTypes = {
-    counter: PropTypes.number,
-    increment: PropTypes.func,
-  }
-
-  state = {
-    data: [],
-  }
-
-  componentDidMount() {
-    axios.get('/api/projects')
-      .then((data) => this.setState(data))
-  }
-
-  incrementCounter = () => {
-    const { increment, counter } = this.props
-
-    increment(counter + 1)
-  }
-
   render() {
-    const { data } = this.state
-    const { counter, increment } = this.props
-
     return (
-      <div>
-        App
-        response:
-        <br />
-        {
-          data.map((project) => (
-            <span key={project.id}> {project.name} </span>
-          ))
-        }
-        <div>
-          Counter: {counter}
-          <button onClick={this.incrementCounter}>Increment</button>
+      <BrowserRouter>
+        <div className="container">
+          <Route path="/" component={MainPage} exact />
+          <Route path="/login" component={LoginPage} exact />
         </div>
-      </div>
+      </BrowserRouter>
     )
   }
-
 }
