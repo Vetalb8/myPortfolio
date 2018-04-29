3810b454d511aa07832ad43ebd7c2b3df8e6a8a9
Vitaliy Kudla <vkudla@scentbird.com>
2018-04-29 21:55:14 +0400

    added media mixin, vw mixin and grayscale, added height 100%

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import './App.scss'
@@ -12,10 +12,10 @@ export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="container">
+        <Fragment>
           <Route path="/" component={MainPage} exact />
           <Route path="/login" component={LoginPage} exact />
-        </div>
+        </Fragment>
       </BrowserRouter>
     )
   }
